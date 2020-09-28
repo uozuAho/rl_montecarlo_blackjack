b279658af83a9df92b0eb45fb1e3dafd61e7f5b1
Warwick Stone <uozu.aho@gmail.com>
2020-09-28 22:14:32 +1000

    mc optimiser: extract ActionValues and Returns

--- ai_blackjack/monte_carlo_optimal_policy.py
+++ ai_blackjack/monte_carlo_optimal_policy.py
@@ -22,6 +22,32 @@ def run_demo():
     # )
 
 
+def find_optimal_policy():
+    gamma = 1.0
+    policy = MutableAgent()
+
+    action_values: ActionValues()
+    returns: Returns()
+
+    while False:
+        state = bj.State(0, 0, False) # todo: random state
+        action = 0 # todo: random action
+        G_return = 0
+        # todo: generate episode with given starting state and action (exploring start)
+        episode = bj.Episode(list(generate_episode(state, action)))
+        for t in reversed(range(episode.length() - 1)):
+            state = episode.steps[t].state
+            action = episode.steps[t].action
+            G_return = gamma * G_return + episode.steps[t + 1].reward
+            if episode.is_first_visit(state, t): # todo: is first visit for state and action
+                returns.add(state, action, G_return)
+                action_values.add(state, action, returns.average_for(state, action))
+                best_action = action_values.highest_value_action(state)
+                policy.set_action(state, best_action)
+
+    return policy, {bj.State(0, 0, False): 0.0}
+
+
 class MutableAgent:
     def action(self, state):
         return 0
@@ -30,8 +56,30 @@ class MutableAgent:
         pass
 
 
-def find_best_action(action_values, state):
-    return 0
+class ActionValues:
+    def __init__(self):
+        self._values = Dict[Tuple[bj.State, int], float] = {}
+
+    def add(self, state: bj.State, action: int, value: float):
+        self._values[(state, action)] = value
+
+    def highest_value_action(self, state: bj.State):
+        return 0
+
+
+class Returns:
+    def __init__(self):
+        self._returns = Dict[Tuple[bj.State, int], float] = {}
+
+    def add(self, state: bj.State, action: int, return_: float):
+        state_action = (state, action)
+        if state_action in self._returns:
+            self._returns[state_action].append(return_)
+        else:
+            self._returns[state_action] = [return_]
+
+    def average_for(self, state: bj.State, action: int):
+        return avg(self._returns[(state, action)])
 
 
 def generate_episode(first_state, first_action):
@@ -41,37 +89,3 @@ def generate_episode(first_state, first_action):
 def avg(things):
     list_of_things = list(things)
     return sum(list_of_things) / len(list_of_things)
-
-
-def find_optimal_policy():
-    gamma = 1.0
-    policy = MutableAgent()
-
-    action_values: Dict[Tuple[bj.State, int], float] = {}
-    returns:       Dict[Tuple[bj.State, int], float] = {}
-
-    while False:
-        state = bj.State(0, 0, False) # todo: random state
-        action = 0 # todo: random action
-        G_return = 0
-        episode = bj.Episode(list(generate_episode(state, action))) # todo: generate episode
-        for t in reversed(range(episode.length() - 1)):
-            state = episode.steps[t].state
-            action = episode.steps[t].action
-            G_return = gamma * G_return + episode.steps[t + 1].reward
-            if episode.is_first_visit(state, t): # todo: is first visit for state and action
-                state_action = (state, action)
-                if state_action in returns:
-                    returns[state_action].append(G_return)
-                else:
-                    returns[state_action] = [G_return]
-
-                action_values[state_action] = avg(returns[state_action])
-                best_action = find_best_action(action_values, state)
-                policy.set_action(state, best_action)
-
-    return policy, {bj.State(0, 0, False): 0.0}
-
-
-if __name__ == "__main__":
-    run_demo()
